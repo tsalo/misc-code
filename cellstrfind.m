557da650b3446cfbf256fe7fd074d5bdbda2602c
Taylor Salo <tsalo90@gmail.com>
2014-09-15 10:18:32 -0700

    Updating cellstrfind to work with save_clusters_and_effect_size.

--- cellstrfind.m
+++ cellstrfind.m
@@ -1,13 +1,25 @@
-function K = cellstrfind(string, patternCells)
-% FORMAT K = cellstrfind(string, patternCells)
+function K = cellstrfind(string, patternCells, setting)
+% FORMAT K = cellstrfind(string, patternCells, setting)
 % Just tells you if any of the string patterns in a cell array are in a
 % string.
-% K (output): 0 if no match, 1 if match.
+% 
+%
+% string:           String to be checked.
+% patternCells:     Cell array of strings to be compared against string.
+% setting:          "Exact" or anything else. String. Determines if
+%                   cellstrfind will use strmatch or strfind.
+%
+% K (output):       0 if no match, 1 if match.
 K = 0;
 for iPattern = 1:length(patternCells)
-    if strfind(string, patternCells{iPattern})
-        K = 1;
+    if strcmp(setting, 'exact')
+        if strcmp(string, patternCells{iPattern})
+            K = 1;
+        end
+    else
+        if strfind(string, patternCells{iPattern})
+            K = 1;
+        end
     end
 end
 end
-
