39eb536d7ca7dc60c5f3b10d539ca8c002c4657e
Taylor Salo <tsalo90@gmail.com>
2014-10-20 23:08:30 -0700

    Adding time_elapsed, a python function that acts the same of my Matlab function hourToc. Simply use with datetime.now() to print out the time elapsed in a nice format.

---
+++ time_elapsed.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 20 23:01:20 2014
+
+@author: taylorsalo
+"""
+from datetime import datetime
+
+def main(start_time):
+    time_elapsed = datetime.now() - start_time
+    seconds_elapsed = time_elapsed.total_seconds()
+    if seconds_elapsed >= 3600:
+        hours_elapsed = seconds_elapsed / 3600
+        seconds_left = seconds_elapsed % 3600
+        minutes_elapsed = seconds_left / 60
+        seconds_left = seconds_left % 60
+        print("%d hours, %d minutes, and %d seconds elapsed." %
+              (hours_elapsed, minutes_elapsed, seconds_left))
+    elif seconds_elapsed >= 60:
+        minutes_elapsed = seconds_elapsed / 60
+        seconds_left = seconds_elapsed % 60
+        print("%d minutes and %d seconds elapsed." % (minutes_elapsed,
+                                                      seconds_left))
+    else:
+        print("%d seconds elapsed." % seconds_elapsed)
