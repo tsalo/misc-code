f52d840ff3fbda83fe087f5aaa30ae0c631c2590
Taylor Salo <taylor.salo@ucdmc.ucdavis.edu>
2015-01-03 14:18:06 -0800

    Update cellstrfind.m

--- cellstrfind.m
+++ cellstrfind.m
@@ -3,13 +3,16 @@ function K = cellstrfind(string, patternCells, setting)
 % Just tells you if any of the string patterns in a cell array are in a
 % string.
 % 
-%
+% Inputs:
 % string:           String to be checked.
 % patternCells:     Cell array of strings to be compared against string.
 % setting:          "Exact" or anything else. String. Determines if
 %                   cellstrfind will use strmatch or strfind.
 %
-% K (output):       0 if no match, 1 if match.
+% Outputs:
+% K:                0 if no match, 1 if match.
+%
+% 140101 Created by Taylor Salo
 K = 0;
 for iPattern = 1:length(patternCells)
     if exist('setting', 'var') && strcmp(setting, 'exact')
