cc2a90ee9afd6dbb856374b5ce27c021fbc1faa7
Taylor Salo <taylor.salo@ucdmc.ucdavis.edu>
2015-01-03 13:57:59 -0800

    Update convertPs.m

--- convertPs.m
+++ convertPs.m
@@ -1,27 +1,27 @@
-function convertPs(filename, out_type)
-% FORMAT convertPs(filename, out_type)
+function convertPs(filename, outType)
+% FORMAT convertPs(filename, outType)
 % A small function that calls GhostScript to convert postscript files to
 % other formats. The default is pdf.
 %
 %
 % filename: The .ps file to convert (include .ps extension). String.
-% out_type: The format to which the postscript file will be converted.
+% outType: The format to which the postscript file will be converted.
 %           String. Possible values: jpg, pdf. Others will need to be
 %           added to the code.
 
-if ~exist('out_type', 'var')
-    out_type = 'pdf';
+if ~exist('outType', 'var')
+    outType = 'pdf';
     device = 'pdfwrite';
 end
-[~,filename_short,~] = fileparts(filename);
+[~, filenameShort, ~] = fileparts(filename);
 
-if strcmp(out_type, 'jpg')
+if strcmp(outType, 'jpg')
     device = 'jpeg';
 elseif strcmp(out_type, 'pdf')
     device = 'pdfwrite';
 end
 
 system(['gs -dBATCH -dNOPAUSE -sDEVICE=' device ' -sOutputFile=' filename_short '.' out_type ' -r100 ' filename])
-fprintf('Converted %s to %s.%s\n', filename, filename_short, out_type);
+fprintf('Converted %s to %s.%s\n', filename, filenameShort, outType);
 
 end
