216992ab2cc424903ae7756ecfabfd8ea5850e51
Taylor Salo <taylor.salo@ucdmc.ucdavis.edu>
2015-01-03 14:15:11 -0800

    Update removeEmptyCells.m

--- removeEmptyCells.m
+++ removeEmptyCells.m
@@ -2,6 +2,16 @@ function [outCellArray] = removeEmptyCells(cellArray)
 % FORMAT [outCellArray] = removeEmptyCells(cellArray)
 % A very simple function to remove empty cells from cell arrays. Great for
 % subject lists.
+%
+% Inputs:
+% cellArray:    Cell array or cell array of structures in format outputted
+%               by readCsv.
+%
+% Outputs:
+% outCellArray: cellArray without empty cells.
+%
+%
+% 140804 Created by Taylor Salo
 
 for iCell = 1:length(cellArray)
     cellArray{iCell} = cellArray{iCell}(~isspace(cellArray{iCell}));
