6f61f7d1ad0648f4e1937a10425703a622faabdc
Taylor Salo <tsalo90@gmail.com>
2015-01-19 22:29:38 -0800

    Add populate_infrastructure.py.

---
+++ populate_infrastructure.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 19 21:40:45 2015
+Copies new infrastructure string from INFRASTRUCTURE.md in specified folder and
+populates changes across all README.md files in folder and subfolders.
+@author: taylorsalo
+"""
+
+import os
+
+infrastructure_file = "/Users/taylorsalo/TCANLab/general-tools/INFRASTRUCTURE.md"
+start_line = "<!-- Start infrastructure -->"
+end_line = "<!-- End infrastructure -->"
+
+with open(infrastructure_file, "r") as fo:
+    infrastructure = [line.rstrip('\n') for line in fo]
+
+folder, _ = os.path.split(infrastructure_file)
+
+for root, dirs, files in os.walk(folder):
+    for file_ in files:
+        if file_ == "README.md":
+            with open(root + "/" + file_, "r") as fo:
+                readme_data = [line.rstrip('\n') for line in fo]
+
+            start_index = readme_data.index(start_line)
+            end_index = readme_data.index(end_line)
+            del(readme_data[start_index:end_index + 1])
+            readme_data[start_index:start_index] = infrastructure
+
+            with open(root + "/" + file_, "w") as fo:
+                for item in readme_data:
+                    fo.write("%s\n" % item)
