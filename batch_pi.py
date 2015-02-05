8b99bded6bd4973c1428442f85cfbf518dfd338f
Taylor Salo <tsalo90@gmail.com>
2015-02-05 13:19:43 -0800

    Update populate_infrastructure.py.

---
+++ batch_pi.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 03 10:25:40 2015
+Sample wrapper for populate_infrastructure.py.
+@author: tsalo
+"""
+import populate_infrastructure as pi
+
+infrastructure_file = "C:/Users/tsalo/Documents/TCANLab/task-tools/INFRASTRUCTURE.md"
+pi.main(infrastructure_file)
--- populate_infrastructure.py
+++ populate_infrastructure.py
@@ -9,39 +9,44 @@ README has been moved recently.
 Caveat: INFRASTRUCTURE.md must be in the base folder of the repository.
 @author: taylorsalo
 """
-
 import os
+import sys
 
-infrastructure_file = "C:/Users/tsalo/Documents/TCANLab/task-tools/INFRASTRUCTURE.md"
-repository_name = os.path.basename(os.path.normpath(os.path.dirname(infrastructure_file)))
 
-start_line = "<!-- Start infrastructure -->"
-end_line = "<!-- End infrastructure -->"
+def main(infrastructure_file):
+    repository_name = os.path.basename(os.path.normpath(os.path.dirname(infrastructure_file)))
+    
+    start_line = "<!-- Start infrastructure -->"
+    end_line = "<!-- End infrastructure -->"
+    
+    remove_string = "tree/master/"
+    
+    with open(infrastructure_file, "r") as fo:
+        infrastructure = [line.rstrip('\n') for line in fo]
+    
+    folder, _ = os.path.split(infrastructure_file)
+    
+    for root, dirs, files in os.walk(folder):
+        for file_ in files:
+            if file_ == "README.md":
+                with open(root + "/" + file_, "r") as fo:
+                    readme_data = [line.rstrip('\n') for line in fo]
+    
+                start_index = readme_data.index(start_line)
+                end_index = readme_data.index(end_line)
+                del(readme_data[start_index:end_index + 1])
+                readme_data[start_index:start_index] = infrastructure
+                
+                repo_name_index = root.index(repository_name)
+                folder_name = os.path.normpath(root[repo_name_index:]).replace("\\", "/")
+                new_path = folder_name.replace(remove_string, "")
+                del(readme_data[0])
+                readme_data[0:0] = [new_path]
+    
+                with open(root + "/" + file_, "w") as fo:
+                    for item in readme_data:
+                        fo.write("%s\n" % item)
 
-remove_string = "tree/master/"
 
-with open(infrastructure_file, "r") as fo:
-    infrastructure = [line.rstrip('\n') for line in fo]
-
-folder, _ = os.path.split(infrastructure_file)
-
-for root, dirs, files in os.walk(folder):
-    for file_ in files:
-        if file_ == "README.md":
-            with open(root + "/" + file_, "r") as fo:
-                readme_data = [line.rstrip('\n') for line in fo]
-
-            start_index = readme_data.index(start_line)
-            end_index = readme_data.index(end_line)
-            del(readme_data[start_index:end_index + 1])
-            readme_data[start_index:start_index] = infrastructure
-            
-            repo_name_index = root.index(repository_name)
-            folder_name = os.path.normpath(root[repo_name_index:]).replace("\\", "/")
-            new_path = folder_name.replace(remove_string, "")
-            del(readme_data[0])
-            readme_data[0:0] = [new_path]
-
-            with open(root + "/" + file_, "w") as fo:
-                for item in readme_data:
-                    fo.write("%s\n" % item)
+if __name__ == "__main__":
+    main(sys.argv[1])
